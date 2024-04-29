91e55c07a9465e7102d6e9dee83f9a624a78b2f9
Lukas Bielesch <bielesch.lukas@gmail.com>
2024-04-29 23:51:16 +0200

    Add Greeting example

---
+++ hw-01/src/greeting.rs
@@ -0,0 +1,12 @@
+use std::io;
+
+fn main() {
+    println!("Enter your name:");
+    let mut name = String::new();
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Failed to read line");
+    let name = name.trim();
+
+    println!("Hello, {}!", name);
+}
