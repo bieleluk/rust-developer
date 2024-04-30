0288ae52318799ded355569b83d39d74bf36004f
Lukas Bielesch <bielesch.lukas@gmail.com>
2024-05-01 00:53:56 +0200

    Read number of arguments first

--- hw-02/src/main.rs
+++ hw-02/src/main.rs
@@ -4,24 +4,25 @@ use std::io;
 
 fn main() {
     let mut line = String::new();
-    println!("Enter a string:");
-    // Read a string from stdin
-    io::stdin()
-        .read_line(&mut line)
-        .expect("Failed to read line");
-    let line = line.trim();
-    println!("Original: {line}");
 
     // Parse cli arguments
     let args: Vec<String> = env::args().collect();
-    if args.len() == 2 {
+    if args.len() != 2 {
+        println!("Wrong number of arguments!")
+    } else {
+        println!("Enter a string:");
+        // Read a string from stdin
+        io::stdin()
+            .read_line(&mut line)
+            .expect("Failed to read line");
+        let line = line.trim();
+        println!("Original: {line}");
+
         let transformed = transform_string(&line, &args[1]);
         match transformed {
             Some(result) => println!("Transformed result: {}", result),
             None => println!("Unknown argument!"),
         }
-    } else {
-        println!("Wrong number of arguments!")
     }
 }
 
