f8c0cda72593022feb0535f428c2963dc633239b
Lukas Bielesch <bielesch.lukas@gmail.com>
2024-05-13 23:08:00 +0200

    Switch option to result, temporarily comment-out the tests

--- hw-l05-errors/src/main.rs
+++ hw-l05-errors/src/main.rs
@@ -1,29 +1,19 @@
 use std::env::args;
-use std::io;
 
 mod str_utils;
-use str_utils::transform;
+use str_utils::collect_and_transform;
 
 fn main() {
-    let mut line = String::new();
-
     // Parse cli arguments
     let args: Vec<String> = args().collect();
     if args.len() != 2 {
         eprintln!("Wrong number of arguments!");
     } else {
-        println!("Enter a string:");
-        // Read a string from stdin
-        io::stdin()
-            .read_line(&mut line)
-            .expect("Failed to read line");
-        let line = line.trim();
-        println!("Original: {line}");
+        let result = collect_and_transform(&args[1]);
 
-        let transformed = transform(&line, &args[1]);
-        match transformed {
-            Some(result) => println!("Transformed result: {}", result),
-            None => eprintln!("Unknown argument!"),
+        match result {
+            Ok(result) => println!("Transformed result: {}", result),
+            Err(e) => eprintln!("Error {e} occurred!"),
         }
     }
 }
