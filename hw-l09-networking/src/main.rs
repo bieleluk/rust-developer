6df88046b2e8a08c26c577560b216e5be035778c
Lukas Bielesch <bielesch.lukas@gmail.com>
2024-05-26 20:16:41 +0200

    Create a simple client-server

--- hw-l09-networking/src/main.rs
+++ hw-l09-networking/src/main.rs
@@ -1,3 +1,22 @@
+use std::thread;
+use networking::{start_server, start_client};
+use std::net::Ipv4Addr;
+
 fn main() {
-    println!("Hello, world!");
+    let ip: Ipv4Addr = Ipv4Addr::new(127,0,0,1);
+    let port: u16 = 11111;
+
+    // Start the server in a separate thread
+    let server_handle = thread::spawn(move || {
+        let _ = start_server(Some(ip), Some(port));
+    });
+
+    // Give the server some time to start
+    thread::sleep(std::time::Duration::from_secs(1));
+
+    // Start the client
+   let _ = start_client(Some(ip), Some(port));
+
+    // Wait for the server thread to finish (it won't in this example)
+    server_handle.join().unwrap();
 }
