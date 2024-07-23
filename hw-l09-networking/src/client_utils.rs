9ba4f7c99d06f79dd18def725c3d9d0540067371
Lukas Bielesch <bielesch.lukas@gmail.com>
2024-07-23 22:51:57 +0200

    Split into client and server parts

---
+++ hw-l09-networking/src/client_utils.rs
@@ -0,0 +1,83 @@
+
+use std::error::Error;
+use std::io::{stdin, Read, Write};
+use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4, TcpStream};
+use crate::MessageType;
+
+pub fn start_client(ip: Option<Ipv4Addr>, port: Option<u16>) -> Result<(), Box<dyn Error>> {
+    let stream = create_client(ip, port)?;
+    client_loop(stream)?;
+    Ok(())
+}
+
+pub fn create_client(ip: Option<Ipv4Addr>, port: Option<u16>) -> Result<TcpStream, Box<dyn Error>> {
+    // Use the provided values or default to localhost and port 11111
+    let ip = ip.unwrap_or(Ipv4Addr::LOCALHOST);
+    let port = port.unwrap_or(11111);
+
+    let sock_addr = SocketAddr::V4(SocketAddrV4::new(ip, port));
+    let stream = TcpStream::connect(sock_addr)?;
+    println!("Client connected to {sock_addr}");
+    Ok(stream)
+}
+
+fn client_loop(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
+    loop {
+        // Read user input from stdin
+        let mut input = String::new();
+        stdin().read_line(&mut input)?;
+
+        // Send request to server
+        let request = input.trim().as_bytes();
+        stream.write_all(request)?;
+
+        let response = receive_response(&mut stream)?;
+        match response {
+            MessageType::Text(text) => {
+                println!("Received text: {text}");
+            }
+            MessageType::Image(_) => {
+                println!("Received image...");
+                response.to_image()?;
+            }
+            MessageType::File { ref name, content: _ } => {
+                println!("Received file {name}");
+                response.to_file()?;
+            }
+            MessageType::Quit => {
+                println!("Quitting");
+                return Ok(());
+            }
+        }
+    }
+}
+
+fn receive_response(stream: &mut TcpStream) -> Result<MessageType, Box<dyn Error>> {
+    let mut buffer = Vec::new();
+
+    loop {
+        // Read data from the stream into a temporary buffer
+        let mut temp_buffer = [0; 100];
+        let bytes_read = match stream.read(&mut temp_buffer) {
+            Ok(0) => return Err("Connection closed".into()), // Connection closed
+            Ok(n) => n,
+            Err(e) => return Err(e.into()),
+        };
+
+        // Append the read data to the main buffer
+        buffer.extend_from_slice(&temp_buffer[..bytes_read]);
+
+        // Attempt to deserialize the buffer
+        match bincode::deserialize::<MessageType>(&buffer) {
+            Ok(message) => {
+                // Return the successfully deserialized message
+                return Ok(message);
+            }
+            Err(_) => {
+                // If deserialization fails, it might be due to incomplete data,
+                // so continue reading more data from the stream
+                // println!("Failed to deserialize message, continue reading");
+            }
+        }
+    }
+}
---
+++ hw-l09-networking/src/server_utils.rs
@@ -0,0 +1,88 @@
+use std::error::Error;
+use std::io::{Read, Write};
+use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4, TcpListener, TcpStream};
+use std::path::Path;
+use crate::MessageType;
+
+
+pub fn start_server(ip: Option<Ipv4Addr>, port: Option<u16>) -> Result<(), Box<dyn Error>> {
+    let server = create_server(ip, port)?;
+    server_loop(server)?;
+    println!("Server execution finished");
+
+    Ok(())
+}
+
+fn create_server(ip: Option<Ipv4Addr>, port: Option<u16>) -> Result<TcpListener, Box<dyn Error>> {
+    // Use the provided values or default to localhost and port 8080
+    let ip = ip.unwrap_or(Ipv4Addr::LOCALHOST);
+    let port = port.unwrap_or(11111);
+
+    let sock_addr = SocketAddr::V4(SocketAddrV4::new(ip, port));
+    let listener = TcpListener::bind(sock_addr)?;
+    println!("Listener {sock_addr} created");
+    Ok(listener)
+}
+
+fn server_loop(listener: TcpListener) -> Result<(), Box<dyn Error>> {
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => match handle_client(stream) {
+                Ok(_) => {}
+                Err(e) => {
+                    println!("Handling of client finished with error: {}", e)
+                }
+            },
+            Err(e) => {
+                eprintln!("Failed to accept connection: {}", e);
+            }
+        }
+    }
+    Ok(())
+}
+
+fn handle_client(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
+    loop {
+        let request = receive_request(&mut stream)?;
+        let response = create_response(&request)?;
+        send_response(&response, &mut stream)?;
+        if let MessageType::Quit = response {
+            stream.shutdown(std::net::Shutdown::Both)?;
+            return Ok(());
+        }
+    }
+}
+
+fn receive_request(stream: &mut TcpStream) -> Result<String, Box<dyn Error>> {
+    let mut buffer = [0; 100];
+    let n = match stream.read(&mut buffer) {
+        Ok(0) => return Err("Connection closed".into()), // Connection closed
+        Ok(n) => n,
+        Err(e) => return Err(e.into()),
+    };
+
+    let request = String::from_utf8(buffer[..n].to_vec())?;
+    Ok(request)
+}
+
+fn create_response(input: &String) -> Result<MessageType, Box<dyn Error>> {
+    // Create a message
+    let message = if input.starts_with(".quit") {
+        Ok(MessageType::Quit)
+    } else if input.starts_with(".file ") {
+        let path = Path::new(input.trim_start_matches(".file ").trim());
+        MessageType::from_file(path)
+    } else if input.starts_with(".image ") {
+        let path = Path::new(input.trim_start_matches(".image ").trim());
+        MessageType::from_image(path)
+    } else {
+        MessageType::from_text(input)
+    }?;
+    Ok(message)
+}
+
+fn send_response(message: &MessageType, stream: &mut TcpStream) -> Result<(), Box<dyn Error>> {
+    let encoded: Vec<u8> = bincode::serialize(message).unwrap();
+    stream.write_all(&encoded)?;
+    Ok(())
+}
