aa0cec4b79165be719d83499ca14ca144593d8a4
Lukas Bielesch <bielesch.lukas@gmail.com>
2024-07-24 21:48:40 +0200

    Implement LibError using thiserror for the common crate

--- hw-l13-error-handling/src/bin/client.rs
+++ hw-l13-error-handling/src/bin/client.rs
@@ -10,7 +10,10 @@ fn main() {
 
     // Parse port and ipv4 addr from the arguments
     let args: Vec<String> = env::args().collect();
-    let (ip, port) = parse_addr(&args[1..]);
+    let (ip, port) = parse_addr(&args[1..]).unwrap_or_else(|e| {
+        error!("{e}");
+        std::process::exit(1);
+    });
     info!("Parsed address is: {}:{}", ip, port);
 
     // Start the client
