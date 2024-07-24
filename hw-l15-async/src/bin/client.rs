1342e5b81c9878664552d8fef1b52620b4f9fc45
Lukas Bielesch <bielesch.lukas@gmail.com>
2024-07-24 22:49:51 +0200

    Rewrite client into async

--- hw-l15-async/src/bin/client.rs
+++ hw-l15-async/src/bin/client.rs
@@ -1,29 +1,24 @@
+use anyhow::{Context, Result};
 use env_logger;
-use log::{error, info};
+use log::info;
 use networking::client::start_client;
 use networking::common::parse_addr;
 use std::env;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<()> {
     // Initialize the logger
     env_logger::init();
 
     // Parse port and ipv4 addr from the arguments
     let args: Vec<String> = env::args().collect();
-    let (ip, port) = parse_addr(&args[1..]).unwrap_or_else(|e| {
-        error!("{e}");
-        std::process::exit(1);
-    });
+    let (ip, port) = parse_addr(&args[1..]).context("Failed to parse address")?;
     info!("Parsed address is: {}:{}", ip, port);
 
     // Start the client
-    match start_client(ip, port) {
-        Ok(_) => {
-            info!("Client execution finished without error");
-        }
-        Err(e) => {
-            error!("Client execution finished error: {e}");
-            std::process::exit(1);
-        }
-    }
+    start_client(ip, port)
+        .await
+        .context("Client execution finished error")?;
+    info!("Client execution finished without error");
+    Ok(())
 }
--- hw-l15-async/src/client/mod.rs
+++ hw-l15-async/src/client/mod.rs
@@ -1,31 +1,36 @@
 use crate::common::MessageType;
 use anyhow::{Context, Result};
 use log::{info, trace};
-use std::io::{stdin, Write};
-use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4, TcpStream};
+use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4};
+use tokio::io::{self, AsyncBufReadExt, AsyncWriteExt, BufReader};
+use tokio::net::TcpStream;
 
 /// Starts the client, connecting to the specified IP and port.
-pub fn start_client(ip: Ipv4Addr, port: u16) -> Result<()> {
+pub async fn start_client(ip: Ipv4Addr, port: u16) -> Result<()> {
     // Create the client stream
-    let stream = create_client(ip, port).context("Failed to create client")?;
+    let stream = create_client(ip, port)
+        .await
+        .context("Failed to create client")?;
     // Start the client loop to handle communication with the server
-    client_loop(stream).context("Client loop crashed")?;
+    client_loop(stream).await.context("Client loop crashed")?;
     Ok(())
 }
 
 /// Creates a TcpStream to connect to the specified IP and port.
-pub fn create_client(ip: Ipv4Addr, port: u16) -> Result<TcpStream> {
+pub async fn create_client(ip: Ipv4Addr, port: u16) -> Result<TcpStream> {
     // Use the provided IP and port to create the socket address
     let sock_addr = SocketAddr::V4(SocketAddrV4::new(ip, port));
     trace!("Connecting..."); // Trace log for connection attempt
-    let stream = TcpStream::connect(sock_addr).context("Failed to connect to server")?; // Connect to the server
+    let stream = TcpStream::connect(sock_addr)
+        .await
+        .context("Failed to connect to server")?; // Connect to the server
     trace!("Local address: {}", stream.local_addr().unwrap()); // Trace log for local address
     info!("Connected to {sock_addr}"); // Info log for successful connection
     Ok(stream)
 }
 
 /// Main loop to handle communication with the server.
-fn client_loop(mut stream: TcpStream) -> Result<()> {
+async fn client_loop(mut stream: TcpStream) -> Result<()> {
     info!(
         "Use one of the following requests:
     .image <image.png>
@@ -34,20 +39,31 @@ fn client_loop(mut stream: TcpStream) -> Result<()> {
 Any other will be returned as a plain text"
     );
 
+    let stdin = io::stdin();
+    let mut reader = BufReader::new(stdin);
+
     loop {
         // Read user input from stdin
         info!("Insert the request");
         let mut input = String::new();
-        stdin().read_line(&mut input).context("Failed to read a line from stdin")?;
+        reader
+            .read_line(&mut input)
+            .await
+            .context("Failed to read a line from stdin")?;
 
         // Send the request to the server
         let request = input.trim().as_bytes();
         trace!("Sending request: {:?}", request);
-        stream.write_all(request).context("Requset sending failed")?;
+        stream
+            .write_all(request)
+            .await
+            .context("Requset sending failed")?;
         trace!("Waiting for the response");
 
         // Receive the response from the server
-        let response = MessageType::receive(&mut stream).context("Response receiving failed")?;
+        let response = MessageType::receive(&mut stream)
+            .await
+            .context("Response receiving failed")?;
         // Take action based on the response
         match response {
             MessageType::Text(text) => {
@@ -55,14 +71,14 @@ Any other will be returned as a plain text"
             }
             MessageType::Image(_) => {
                 info!("Received image...");
-                response.to_image()?;
+                response.to_image().await?;
             }
             MessageType::File {
                 ref name,
                 content: _,
             } => {
                 info!("Received file {name}");
-                response.to_file()?;
+                response.to_file().await?;
             }
             MessageType::Quit => {
                 info!("Quitting");
