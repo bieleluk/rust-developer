5ad6720f664eac363f945bd552d8d760103e76ca
Lukas Bielesch <bielesch.lukas@gmail.com>
2024-07-24 18:13:15 +0200

    Unify repetitive address parsing

--- hw-l11-ecosystem/src/bin/client.rs
+++ hw-l11-ecosystem/src/bin/client.rs
@@ -1,37 +1,20 @@
 use env_logger;
-use log::{error, info, trace};
+use log::{error, info};
 use networking::client::start_client;
+use networking::common::parse_addr;
 use std::env;
-use std::net::Ipv4Addr;
 
 fn main() {
     // Initialize the logger
     env_logger::init();
 
+    // Parse port and ipv4 addr from the arguments
     let args: Vec<String> = env::args().collect();
-    trace!("Number of arguments: {}, arguments: {:?}", args.len(), args);
-
-    // Set default IP address and port
-    let mut ip = Ipv4Addr::LOCALHOST;
-    let mut port = 11111;
-    trace!("Default socket addr: {}:{}", ip, port);
-
-    // Update port and IP address based on provided arguments
-    if args.len() > 1 {
-        port = args[1].parse::<u16>().expect("Error parsing port");
-    } else {
-        trace!("Using default port number")
-    }
-    if args.len() > 2 {
-        ip = args[2]
-            .parse::<Ipv4Addr>()
-            .expect("Error parsing ip address");
-    } else {
-        trace!("Using default ipv4 address");
-    }
+    let (ip, port) = parse_addr(&args[1..]);
+    info!("Parsed address is: {}:{}", ip, port);
 
     // Start the client
-    match start_client(Some(ip), Some(port)) {
+    match start_client(ip, port) {
         Ok(_) => {
             info!("Client execution finished without error")
         }
--- hw-l11-ecosystem/src/lib.rs
+++ hw-l11-ecosystem/src/lib.rs
@@ -1,5 +1,4 @@
 
 pub mod client;
 pub mod server;
-
-mod common;
+pub mod common;
